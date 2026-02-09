3ff0469fe16398e145781002555bfed733282563
Junaid <junaiduthman123@gmail.com>
2026-02-09 21:15:45 +0100

    test webhook

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,4 @@
-import { bootstrapApplication } from '@angular/platform-browser';//test webhook
+import { bootstrapApplication } from '@angular/platform-browser';//test  webhook
 import { AppComponent } from './app/app.component';
 import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './app/interceptors/auth.interceptor';
