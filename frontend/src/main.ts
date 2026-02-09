0a1a944033f44b60678dd5a786cd2a59d32fb46d
Junaid <junaiduthman123@gmail.com>
2026-02-09 18:24:10 +0100

    add good code test 5

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,4 @@
-import { bootstrapApplication } from '@angular/platform-browser';//test comment(4)
+import { bootstrapApplication } from '@angular/platform-browser';//test comment(5))
 import { AppComponent } from './app/app.component';
 import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './app/interceptors/auth.interceptor';
